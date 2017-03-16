d39bfb191ebe148bb5bb6b24e447ef5d2448ab1b
Kryszak <prajskrzysztof@gmail.com>
2017-03-16 19:43:17 +0100

    Reporter impl

---
+++ Bank/Reporter.cs
@@ -0,0 +1,16 @@
+﻿using Bank.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bank
+{
+    class Reporter : IReporter
+    {
+        public List<BankAccount> CreateReport(List<BankAccount> products, Func<BankAccount, bool> filter)
+        {
+            return products.Where(filter).ToList();
+        }
+    }
+}
