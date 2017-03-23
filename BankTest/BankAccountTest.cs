7473207c56b1d9a6088bd23a8f7a3ce222dcc7da
Kamil Kazmierczak <kamil.kazmierczak@hotmail.com>
2017-03-23 18:55:42 +0100

    fix last tests

--- BankTest/BankAccountTest.cs
+++ BankTest/BankAccountTest.cs
@@ -9,16 +9,18 @@ namespace BankTest
     {
         private Bank.Bank _bank = null;
         private Bank.Products.BankAccount _bankAccount = null;
+        private const double ToWithdraw = 40.31;
+        private const double DepositAmount = 100.52;
 
         public BankAccountTest()
         {
             _bank = new Bank.Bank();
+            _bankAccount = new Bank.Products.BankAccount(_bank, 0, 0);
         }
 
         [Fact]
         public void ShouldCreateBankAccount()
         {
-            _bankAccount = new Bank.Products.BankAccount(_bank, 0, 0);
             Assert.NotNull(_bankAccount);
         }
 
@@ -26,21 +28,17 @@ namespace BankTest
         [Fact]
         public void ShouldDeposit()
         {
-            double amount = 100.52;
-            _bankAccount = new Bank.Products.BankAccount(_bank, 0, 0);
-            _bankAccount.Deposit(amount);
-            Assert.Equal(amount, _bankAccount.GetAccountState());
+            _bankAccount.Deposit(DepositAmount);
+            Assert.Equal(DepositAmount, _bankAccount.GetAccountState());
         }
 
 
         [Fact]
         public void ShouldWithdraw()
         {
-            double amount = 100.0;
-            _bankAccount = new Bank.Products.BankAccount(_bank, 0, 0);
-            _bankAccount.Deposit(amount);
-            _bankAccount.Withdraw(40.0);
-            Assert.Equal(60.0, _bankAccount.GetAccountState());
+            _bankAccount.Deposit(DepositAmount);
+            _bankAccount.Withdraw(ToWithdraw);
+            Assert.Equal(DepositAmount - ToWithdraw, _bankAccount.GetAccountState());
         }
 
 
