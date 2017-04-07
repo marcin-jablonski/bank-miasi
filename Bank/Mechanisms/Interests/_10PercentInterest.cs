69384b43fae1c2d54bc801c4e3e09725e8af9f8e
Kryszak <prajskrzysztof@gmail.com>
2017-04-07 12:03:35 +0200

    3ci mechanizm odsetek

---
+++ Bank/Mechanisms/Interests/_10PercentInterest.cs
@@ -0,0 +1,18 @@
+﻿using Bank.Interfaces;
+
+namespace Bank.Mechanisms.Interests
+{
+    public class _10PercentInterest : IInterest
+    {
+        private const double InterestPercentBelowLimit = 1.1;
+        private const int InterestAmountLimit = 10000;
+        private const double InterestPercentAboveLimit = 1.05;
+
+        public double ChargeInterest(double amount)
+        {
+            return amount < InterestAmountLimit
+                ? amount * InterestPercentBelowLimit
+                : amount * InterestPercentAboveLimit;
+        }
+    }
+}
