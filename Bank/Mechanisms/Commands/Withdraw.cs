cbfa4325a5f57325d1d87756a11903d18dda9a09
Kryszak <prajskrzysztof@gmail.com>
2017-04-07 13:02:50 +0200

    withdraw prototype

---
+++ Bank/Mechanisms/Commands/Withdraw.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Bank.Enums;
+using Bank.Exceptions;
+using Bank.Interfaces;
+using Bank.Models;
+using Bank.Products;
+
+namespace Bank.Mechanisms.Commands
+{
+    internal class Withdraw : ICommand
+    {
+        private BankAccount _account;
+
+        private double _amount;
+
+        public Withdraw(BankAccount account, double amount)
+        {
+            this._account = account;
+            this._amount = amount;
+        }
+
+        public void Execute()
+        {
+            if (_amount <= 0) throw new IllegalOperationException();
+
+            if (_amount >= _amount)
+                _amount -= _amount;
+            else if (Debit != null)
+            {
+                var toGetFromDebit = _amount - _amount;
+                if (Debit.GetAvailableDebit() >= toGetFromDebit)
+                {
+                    _amount = 0;
+                    Debit.IncreaseDebit(toGetFromDebit);
+                }
+                else
+                {
+                    throw new NotEnoughFundsException();
+                }
+            }
+            else
+            {
+                throw new NotEnoughFundsException();
+            }
+
+            History.Add(new Operation
+            {
+                Type = OperationType.Withdraw,
+                Date = DateTime.Now,
+                Description = _amount.ToString()
+            });
+            Bank.GetHistory()
+                .Add(new Operation
+                {
+                    Type = OperationType.Withdraw,
+                    Date = DateTime.Now,
+                    Description = _amount.ToString()
+                });
+        }
+    }
+}
--- Bank/Products/BankAccount.cs
+++ Bank/Products/BankAccount.cs
@@ -4,6 +4,7 @@ using Bank.Exceptions;
 using Bank.Interfaces;
 using Bank.Models;
 using Bank.Mechanisms;
+using Commands = Bank.Mechanisms.Commands;
 
 namespace Bank.Products
 {
@@ -11,7 +12,7 @@ namespace Bank.Products
     {
         private Debit Debit;
 
-        public BankAccount(Bank bank, int ownerId, IInterest interestSystem) 
+        public BankAccount(Bank bank, int ownerId, IInterest interestSystem)
             : base(bank, ownerId, interestSystem)
         {
         }
@@ -24,7 +25,7 @@ namespace Bank.Products
                 _amount += amount;
             else if (Debit.GetUnpaidDebit() >= 0)
             {
-                if(Debit.GetUnpaidDebit() >= amount)
+                if (Debit.GetUnpaidDebit() >= amount)
                     Debit.ReduceDebit(amount);
                 else
                 {
@@ -34,36 +35,33 @@ namespace Bank.Products
                 }
             }
 
-            History.Add(new Operation {Type = OperationType.Deposit, Date = DateTime.Now, Description = amount.ToString()});
-            Bank.GetHistory().Add(new Operation { Type = OperationType.Deposit, Date = DateTime.Now, Description = amount.ToString() });
+            History.Add(new Operation
+            {
+                Type = OperationType.Deposit,
+                Date = DateTime.Now,
+                Description = amount.ToString()
+            });
+            Bank.GetHistory()
+                .Add(new Operation {Type = OperationType.Deposit, Date = DateTime.Now, Description = amount.ToString()});
         }
 
         public void Withdraw(double amount)
         {
-            if (amount <= 0) throw new IllegalOperationException();
+            new Commands.Withdraw(this, amount).Execute();
 
-            if (_amount >= amount)
-                _amount -= amount;
-            else if (Debit != null)
+            History.Add(new Operation
             {
-                var toGetFromDebit = amount - _amount;
-                if (Debit.GetAvailableDebit() >= toGetFromDebit)
+                Type = OperationType.Withdraw,
+                Date = DateTime.Now,
+                Description = _amount.ToString()
+            });
+            Bank.GetHistory()
+                .Add(new Operation
                 {
-                    _amount = 0;
-                    Debit.IncreaseDebit(toGetFromDebit);
-                }
-                else
-                {
-                    throw new NotEnoughFundsException();
-                }
-            }
-            else
-            {
-                throw new NotEnoughFundsException();
-            }
-
-            History.Add(new Operation { Type = OperationType.Withdraw, Date = DateTime.Now, Description = amount.ToString() });
-            Bank.GetHistory().Add(new Operation { Type = OperationType.Withdraw, Date = DateTime.Now, Description = amount.ToString() });
+                    Type = OperationType.Withdraw,
+                    Date = DateTime.Now,
+                    Description = _amount.ToString()
+                });
         }
 
         public void Transfer(double amount, BankAccount destination)
@@ -78,15 +76,37 @@ namespace Bank.Products
             }
 
             destination.Deposit(amount); // change possibly
-            History.Add(new Operation { Type = OperationType.Transfer, Date = DateTime.Now, Description = amount + " to " + destination.GetId() });
-            Bank.GetHistory().Add(new Operation { Type = OperationType.Transfer, Date = DateTime.Now, Description = amount + " to " + destination.GetId() });
+            History.Add(new Operation
+            {
+                Type = OperationType.Transfer,
+                Date = DateTime.Now,
+                Description = amount + " to " + destination.GetId()
+            });
+            Bank.GetHistory()
+                .Add(new Operation
+                {
+                    Type = OperationType.Transfer,
+                    Date = DateTime.Now,
+                    Description = amount + " to " + destination.GetId()
+                });
         }
 
         public void CreateDebit(Debit debit)
         {
             Debit = debit;
-            History.Add(new Operation { Type = OperationType.DebitCreation, Date = DateTime.Now, Description = debit.GetLimit().ToString() });
-            Bank.GetHistory().Add(new Operation { Type = OperationType.DebitCreation, Date = DateTime.Now, Description = debit.GetLimit().ToString() });
+            History.Add(new Operation
+            {
+                Type = OperationType.DebitCreation,
+                Date = DateTime.Now,
+                Description = debit.GetLimit().ToString()
+            });
+            Bank.GetHistory()
+                .Add(new Operation
+                {
+                    Type = OperationType.DebitCreation,
+                    Date = DateTime.Now,
+                    Description = debit.GetLimit().ToString()
+                });
         }
     }
 }
