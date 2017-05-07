ba9cecbae831350d0347d7d1e77a6a23b3eb03ac
Kamil Kazmierczak <kamil.kazmierczak@hotmail.com>
2017-05-07 18:22:38 +0200

    add Transfer Command

---
+++ Bank/Mechanisms/Commands/Transfer.cs
@@ -0,0 +1,28 @@
+﻿using Bank.Interfaces;
+using Bank.Products;
+using Bank.Mechanisms.Kir;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank.Mechanisms.Commands
+{
+    public class Transfer : ICommand
+    {
+        private BankAccount _source;
+        private BankAccount _destination;
+        private double _amount;
+
+        public Transfer(BankAccount source, BankAccount destination, double amount)
+        {
+            this._source = source;
+            this._destination = destination;
+            this._amount = amount;
+        }
+
+        public void Execute()
+        {
+            Kir.Kir.Transfer(_source.GetIdentificator(), _destination.GetIdentificator(), _amount);
+        }
+    }
+}
